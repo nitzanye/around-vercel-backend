5c4c13b00149d0c1b0df25f93a337dc98ce303e1
NitzanSmulevici <nitzanye@gmail.com>
2023-01-15 17:12:32 +0200

    link to remote mongo

--- app.js
+++ app.js
@@ -1,45 +1,48 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/aroundb', {
+mongoose.connect("mongodb+srv://cluster0.spz2prz.mongodb.net/aroundb", {
   useNewUrlParser: true,
 });
 
-const helmet = require('helmet');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const rateLimit = require('express-rate-limit');
+// mongoose.connect('mongodb://localhost:27017/aroundb', {
+//   useNewUrlParser: true,
+// });
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
 
 // app.use(express.json());
 app.use(bodyParser.json());
 
-require('dotenv').config();
+require("dotenv").config();
 
 const { PORT = 3000 } = process.env;
 
-const { errors } = require('celebrate');
-const { createUser, login } = require('./controllers/users');
-const { pageNotFound } = require('./controllers/page-not-found');
-const auth = require('./middlewares/auth');
-const { requestLogger, errorLogger } = require('./middlewares/logger');
+const { errors } = require("celebrate");
+const { createUser, login } = require("./controllers/users");
+const { pageNotFound } = require("./controllers/page-not-found");
+const auth = require("./middlewares/auth");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 const {
   // authValidation,
   validateUser,
   validateLogin,
-} = require('./middlewares/validations');
+} = require("./middlewares/validations");
 
-const serverErrorHandler = require('./middlewares/server-error-handler');
+const serverErrorHandler = require("./middlewares/server-error-handler");
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // in 15 minutes
   max: 100, // you can make a maximum of 100 requests from one IP
 });
 
-const userRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
+const userRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
 
 app.use(requestLogger);
 
@@ -52,24 +55,24 @@ app.use(helmet());
 // must come before the route handlers
 
 app.use(cors());
-app.options('*', cors());
+app.options("*", cors());
 
 // server crash testing
-app.get('/crash-test', () => {
+app.get("/crash-test", () => {
   setTimeout(() => {
-    throw new Error('Server will crash now');
+    throw new Error("Server will crash now");
   }, 0);
 });
 
 // register and login
-app.post('/signup', validateUser, createUser);
-app.post('/signin', validateLogin, login);
+app.post("/signup", validateUser, createUser);
+app.post("/signin", validateLogin, login);
 
 // authorization
 app.use(auth);
 
-app.use('/', userRouter);
-app.use('/', cardsRouter);
+app.use("/", userRouter);
+app.use("/", cardsRouter);
 
 // must come after the route handlers and before the error handlers
 app.use(errorLogger);
@@ -80,7 +83,7 @@ app.use(errors());
 // centralized error handler
 app.use(serverErrorHandler);
 
-app.use('*', pageNotFound);
+app.use("*", pageNotFound);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
