27f73232493fcb41281e8e2b29f8227b019f131a
NitzanSmulevici <nitzanye@gmail.com>
2023-02-03 12:46:50 +0200

    clean comments

--- db.js
+++ db.js
@@ -4,9 +4,4 @@ const dbName = "test";
 const password = process.env.MONGO_ATLAS_PASSWORD;
 const uri = `mongodb+srv://admin:${password}@cluster0.spz2prz.mongodb.net/${dbName}`;
 
-//const uri = `mongodb+srv://admin:adminAtlasPass@cluster0.spz2prz.mongodb.net/${dbName}`;
 mongoose.connect(uri);
-
-//const uri = `mongodb+srv://admin:${process.env.MONGO_ATLAS_PASSWORD}@cluster.mongodb.net/${dbName}?retryWrites=true&w=majority`;
-
-// mongodb+srv://<username>:<password>@cluster0.spz2prz.mongodb.net/test
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,29 +1,25 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const { NODE_ENV, JWT_SECRET } = process.env;
-const UnauthorizedError = require('../errors/unauthorized-error');
-
-// const extractBearerjwt = (header) => {
-//   return header.replace("Bearer ", "");
-// };
+const UnauthorizedError = require("../errors/unauthorized-error");
 
 const auth = (req, res, next) => {
   // getting authorization from the header
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError('You are not authorized'));
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return next(new UnauthorizedError("You are not authorized"));
   }
 
   let payload;
   // const token = extractBearerjwt(authorization);
-  const token = authorization.replace('Bearer ', '');
+  const token = authorization.replace("Bearer ", "");
   try {
     payload = jwt.verify(
       token,
-      NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+      NODE_ENV === "production" ? JWT_SECRET : "some-secret-key"
     );
   } catch (err) {
-    return next(new UnauthorizedError('You are not authorized'));
+    return next(new UnauthorizedError("You are not authorized"));
   }
 
   // assigning the payload to the request object
