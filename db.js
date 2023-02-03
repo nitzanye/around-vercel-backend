71df9e098d0974450d2cccca37bea765b4332a9c
NitzanSmulevici <nitzanye@gmail.com>
2023-02-03 12:16:33 +0200

    fix dbName

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-const dbName = "aroundb";
+const dbName = "test";
 const password = process.env.MONGO_ATLAS_PASSWORD;
 const uri = `mongodb+srv://admin:${password}@cluster0.spz2prz.mongodb.net/${dbName}`;
 
@@ -8,3 +8,5 @@ const uri = `mongodb+srv://admin:${password}@cluster0.spz2prz.mongodb.net/${dbNa
 mongoose.connect(uri);
 
 //const uri = `mongodb+srv://admin:${process.env.MONGO_ATLAS_PASSWORD}@cluster.mongodb.net/${dbName}?retryWrites=true&w=majority`;
+
+// mongodb+srv://<username>:<password>@cluster0.spz2prz.mongodb.net/test
